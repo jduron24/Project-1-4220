84b9a4993e7a0dab3a00d98766ccda1d61f82f33
Jonathan Duron <jonathanduron@004208-monitor.lib.iastate.edu>
2025-02-11 16:11:06 -0600

    login screen

--- app.py
+++ app.py
@@ -1,8 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from functools import wraps
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '123'
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# The password that will grant access
+MASTER_PASSWORD = "password"  
 
+# Login required decorator
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route('/')
+@login_required
+def home():
+    return 'Welcome to the protected page!'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        password = request.form['password']
+        
+        if password == MASTER_PASSWORD:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid password')
+    
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
